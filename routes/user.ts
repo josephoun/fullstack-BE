c1e8021a399749a69c011b75db5159608235ed06
Joseph Oun <josephoun14@gmail.com>
2021-06-06 12:29:00 +0300

    integrating with mongo client

--- routes/user.ts
+++ routes/user.ts
@@ -1,6 +1,7 @@
 import * as bodyParser from "body-parser";
 import * as express from "express";
 import { Logger } from "../logger/logger";
+import {MongoClient} from "mongodb";
 
 class User {
 
@@ -11,22 +12,50 @@ class User {
     public users: any[];
     public posts: any[];
 
+    uri: string;
+    client: MongoClient;
+
+
     constructor() {
         this.express = express();
         this.middleware();
         this.routes();
-        this.users = [];
+        this.users = [{
+            id: 1234,
+            userName: 'test',
+            password: 'test'
+        }];
         this.posts = [{
             title: 'Weather',
             content: 'The weather is good today'
         }];
         this.logger = new Logger();
+        this.client = new MongoClient(this.uri);
     }
 
     // Configure Express middleware.
     private middleware(): void {
         this.express.use(bodyParser.json());
         this.express.use(bodyParser.urlencoded({ extended: false }));
+
+        async function main() {
+
+            const uri = "mongodb+srv://josephoun14:SolyQb7jejdhTvND@userscluster.pglou.mongodb.net/usersdb?retryWrites=true&w=majority";
+
+            const client = new MongoClient(uri);
+
+            try {
+                // Connect to the MongoDB cluster
+                await client.connect();
+
+            } catch (e) {
+                console.error(e);
+            } finally {
+                await client.close();
+            }
+        }
+
+        main().catch(console.error);
     }
 
     private routes(): void {
@@ -73,7 +102,18 @@ class User {
 
         this.express.post("/authenticate", (req, res, next) => {
             this.logger.info("url [auth]:::::::" + req.url);
-            res.json(this.users);
+            this.logger.info("req body user" + JSON.stringify(req.body));
+
+            this.users.forEach(user => {
+                if (req.body.username === user.userName) {
+                    if (req.body.password === user.password) {
+                        res.json(user)
+                    }
+                }
+            })
+            res.status(403);
+            res.statusMessage = 'forbidden';
+            res.send()
         });
     }
 }
